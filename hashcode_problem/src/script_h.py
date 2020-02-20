0f69d8ef74d34f51c7970cf9359565237284acb8
hildobby <h.moulie@gmail.com>
2020-02-20 19:27:39 +0100

    d

--- hashcode_problem/src/script_h.py
+++ hashcode_problem/src/script_h.py
@@ -1,14 +1,21 @@
-with open ("/Users/hildebert/gits/hashcode/hashcode_problem/src/input/a_example.in", "rt") as input_file:
-    content = input_file.readlines()
+with open ("/Users/hildebert/gits/hashcode/hashcode_problem/src/input/a_example.txt", "rt") as input_file:
+    file_content = input_file.readlines()
 
-for i in range(len(content)):
-    content[i] = content[i].strip("\n")
+for i in range(len(file_content)):
+    file_content[i] = list(map(int, file_content[i].strip("\n").split(' ')))
 
-input_1 = [int(k) for k in content[0].split(' ')]
-input_2 = [int(k) for k in content[1].split(' ')]
+print(len(file_content)/2-1)
 
-print(input_1)
-print(input_2)
+input_data = {}
+input_data['info'] = file_content[0]
+for j in range(int(len(file_content)/2-1)):
+    input_data['library_{}'.format(j)] = {}
+    input_data['library_{}'.format(j)]['book_count'] = file_content[j+1][0]
+    input_data['library_{}'.format(j)]['signup_time'] = file_content[j+1][1]
+    input_data['library_{}'.format(j)]['max_shipment_per_day'] = file_content[j+1][2]
+    input_data['library_{}'.format(j)]['book_scores'] = file_content[j+2]
+
+print(input_data)
 
 with open("/Users/hildebert/gits/hashcode/hashcode_problem/src/output/output_h.txt", "a") as output_file:
     output_file.write('Hello\n')
