013ca3bbea8cd14a60bb12835c7e2a32e77dbaf3
hildobby <h.moulie@gmail.com>
2020-02-20 19:39:15 +0100

    d

--- hashcode_problem/src/script_h.py
+++ hashcode_problem/src/script_h.py
@@ -4,18 +4,15 @@ with open ("/Users/hildebert/gits/hashcode/hashcode_problem/src/input/a_example.
 for i in range(len(file_content)):
     file_content[i] = list(map(int, file_content[i].strip("\n").split(' ')))
 
-input_data = {}
-input_data['total_info'] = {}
-input_data['total_info']['book_count'] = file_content[0][0]
-input_data['total_info']['library_count'] = file_content[0][2]
-input_data['total_info']['dealine'] = file_content[0][1]
+input_data = {'book_count': file_content[0][0],
+              'library_count': file_content[0][1],
+              'deadline': file_content[0][2]}
 
 for j in range(int(len(file_content)/2-1)):
-    input_data['library_{}'.format(j)] = {}
-    input_data['library_{}'.format(j)]['book_count'] = file_content[j+1][0]
-    input_data['library_{}'.format(j)]['signup_time'] = file_content[j+1][1]
-    input_data['library_{}'.format(j)]['max_shipment_per_day'] = file_content[j+1][2]
-    input_data['library_{}'.format(j)]['book_scores'] = file_content[j+2]
+    input_data['library_{}'.format(j)] = {'book_count': file_content[j+1][0],
+                                          'signup_time': file_content[j+1][1],
+                                          'max_shipment_per_day': file_content[j+1][2],
+                                          'book_scores': file_content[j+2]}
 
 print(input_data)
 
